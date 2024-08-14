129ca78805c9b3c51f1c31696b68635a8f63a66a
Irina <irinakhaus@gmail.com>
2024-08-14 22:38:07 +0300

    Add parsing top traders button

--- parser/views.py
+++ parser/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.http import HttpRequest, HttpResponseRedirect
+from seleniumbase import SB
+from .dexscreener_parser.dex_parser import DexScreenerParser
 
-# Create your views here.
+def parse_top_traders(request: HttpRequest):
+    with SB(uc=True, test=True) as sb:
+        dex_parser = DexScreenerParser(sb)
+        dex_parser.parse_top_traders("6yhfwetd7kpubkjetf3doi6iet9qawvtkepxwgh6fbwq")
+    return HttpResponseRedirect('/admin')
