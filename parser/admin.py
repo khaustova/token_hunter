1258c704ff6ad48d1c97bcda19175ebd9eb1234a
Irina <irinakhaus@gmail.com>
2024-08-14 22:36:59 +0300

    Add TopTrader ModelAdmin

--- parser/admin.py
+++ parser/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from django.http import HttpRequest, HttpResponseRedirect
+from django.urls import path
+from .models import TopTrader
 
-# Register your models here.
+
+@admin.register(TopTrader)
+class TopTradersAdmin(admin.ModelAdmin):
+    list_display = ("maker", "coin", "bought", "sold", "PNL")
+    list_per_page = 50
