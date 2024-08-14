0836ed428a0efb2ba79f76d4771c40b7df3d0a98
Irina <irinakhaus@gmail.com>
2024-08-14 22:35:58 +0300

    Add TopTrader model

--- parser/models.py
+++ parser/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+class TopTrader(models.Model):
+    coin = models.CharField(
+        max_length=256, 
+        verbose_name='Монета'
+    )
+    maker = models.CharField(
+        max_length=256, 
+        verbose_name='Кошелёк'
+    )
+    network = models.CharField(
+        max_length=256, 
+        verbose_name='Сеть'
+    )
+    bought = models.IntegerField(
+        verbose_name="Купил"
+    )
+    sold = models.IntegerField(
+        verbose_name="Продал"
+    )
+    PNL = models.FloatField(
+        verbose_name="PNL"
+    )
+    
+    class Meta:
+        verbose_name = 'Топ трейдеры'
+        verbose_name_plural = 'Топ трейдер'
+    
+    def __str__(self):
+        return f'{self.trader}: {self.PNL}'
