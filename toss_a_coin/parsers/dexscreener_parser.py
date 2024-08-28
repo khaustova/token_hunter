0207ad69c1eaa49fdf20002c7a0eb196024452d0
Irina <irinakhaus@gmail.com>
2024-08-28 23:29:37 +0300

    Delete coin parsing

--- toss_a_coin/parsers/dexscreener_parser.py
+++ toss_a_coin/parsers/dexscreener_parser.py
@@ -17,50 +17,6 @@ class DexScreenerParser():
     def __init__(self, sb: SB):
         self.sb = sb
         self.client = DexscreenerClient()
-    
-    def parse_coins(self, pages: int=1, filter: str=""):
-        for page in range(1, pages + 1):
-            self.sb.uc_open_with_reconnect("https://dexscreener.com/solana/page-" + str(page) + filter, reconnect_time=10)
-            if page == 1:
-                self.sb.uc_gui_handle_cf()
-            self.sb.sleep(3)
-            page_sourse = self.sb.get_page_source()
-            self.save_coins(page_sourse)
-            
-    def save_coins(self, page_source):
-        soup = BeautifulSoup(page_source, "html.parser")
-        links = soup.find_all("a", class_="ds-dex-table-row ds-dex-table-row-top")
-        pairs_on_the_page = [a.get("href")[8:] for a in links]
-        
-        temp_df = pd.DataFrame(
-            columns=[
-                "name",
-                "pair_address", 
-                "token_address",
-                "created_date",
-            ]
-        )
-        
-        token_name, pair_addres, token_address, created_at = [], [], [], []   
-
-        for pair in pairs_on_the_page:
-            data = self.client.search_pairs(pair)
-            
-            for token in data: 
-                token_name.append(token.base_token.name)
-                pair_addres.append(token.pair_address)
-                token_address.append(token.base_token.address)
-                created_at.append(token.pair_created_at)
-
-        temp_df = pd.DataFrame(
-            {
-                "name": token_name, 
-                "pair_address": pair_addres,
-                "token_address": token_address,
-                "created_at": created_at
-            }
-        )
-        temp_df.to_csv('output_coin.csv')
                      
     def parse_top_traders_from_the_pages(self, pages: int, filter: str=""):
         for page in range(1, pages + 1):
@@ -174,3 +130,4 @@ class DexScreenerParser():
         number = int(number)
         
         return number
+    
