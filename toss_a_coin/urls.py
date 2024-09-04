20a138c1cc65a982299b77ef5f18a8211bc3e4fa
Irina Khaustova <irinakhaus@gmail.com>
2024-09-05 00:19:48 +0300

    Update urls format

--- toss_a_coin/urls.py
+++ toss_a_coin/urls.py
@@ -1,14 +1,10 @@
 from django.urls import path
-from toss_a_coin.views import (
-    parse_top_traders,
-    check_coin,
-    watch_dexscreener
-)
+from toss_a_coin import views
 
 app_name = "toss_a_coin"
 
 urlpatterns = [
-    path("parse_top_traders/", parse_top_traders, name="parse_top_traders"),
-    path("check_coin/", check_coin, name="check_coin"),
-    path("watch_dexscreener/", watch_dexscreener, name="watch_dexscreener")
+    #path("parse_top_traders/", views.parse_top_traders, name="parse_top_traders"),
+    path("check_coin/", views.check_coin, name="check_coin"),
+    path("watch_dexscreener/", views.watch_dexscreener, name="watch_dexscreener"),
 ]
