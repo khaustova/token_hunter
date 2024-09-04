0d45432d566c9513d87b16a16682094afb11ecf4
Irina Khaustova <irinakhaus@gmail.com>
2024-09-05 00:19:20 +0300

    Replace fetch with post

--- toss_a_coin/views.py
+++ toss_a_coin/views.py
@@ -1,33 +1,49 @@
 import json
 import logging
-from django.http import JsonResponse, HttpRequest
+from django.http import JsonResponse, HttpRequest, HttpResponseRedirect
 from django.conf import settings
-from seleniumbase import SB, Driver
+from django.views.decorators.http import require_POST
+from seleniumbase import SB
+from toss_a_coin.forms import DexscreenerForm
 from .coin_checker import CoinChecker
 from .dexscreener_worker import DexScreeneWatcher
 from .parsers.dexscreener_parser import DexScreenerParser
+from celery import current_app
+from .tasks import watch_dexscreener_task
 
 logger = logging.getLogger(__name__)
 
 
-def parse_top_traders(request: HttpRequest) -> JsonResponse:
-    """
-    Парсит топ кошельки по заданным критериям:
-    filter - фильтр в виде параметров строки запроса;
-    pages - количество страниц.
-    """
-    
-    data = json.loads(request.body)
-    filter = data["filter"] if data["filter"] else ""
-    pages =int(data["pages"]) if data["pages"] else 1
-    
-    with SB(uc=True, test=True, xvfb=True) as sb:
-        dex_parser = DexScreenerParser(sb)
-        dex_parser.parse_top_traders_from_the_pages(pages, filter)
+
+@require_POST
+def watch_dexscreener(request: HttpRequest):
+    form = DexscreenerForm(request.POST)
+    if form.is_valid():
+        
+        if '_parsing' in request.POST:
+            pages = int(form.cleaned_data["pages"]) if form.cleaned_data["pages"] else 1
+            filter = form.cleaned_data["filter"] if form.cleaned_data["filter"] else ""
+            with SB(uc=True, test=True, xvfb=True) as sb:
+                dex_parser = DexScreenerParser(sb)
+                dex_parser.parse_top_traders_from_the_pages(pages, filter)
+                
+        elif '_monitoring' in request.POST:
+            filter = form.cleaned_data["filter"] if form.cleaned_data["filter"] else "?rankBy=trendingScoreH6&order=desc&minLiq=5000&maxAge=1"
+           # watch_dexscreener_task.delay(filter)
             
-    return JsonResponse({"status": "done"})
- 
- 
+            with SB(
+            uc=True, 
+            test=True, 
+            xvfb=True,
+            #headless2=True,
+            extension_dir=settings.CAPTCHA_EXTENSION_DIR
+        ) as sb:
+                dex_parser = DexScreeneWatcher(sb, filter)
+                dex_parser.watch_coins()
+        
+    return HttpResponseRedirect("/admin")
+        
+
 def check_coin(request: HttpRequest) -> JsonResponse:
     """
     Базовая проверка введённой в форму монеты.
@@ -36,26 +52,4 @@ def check_coin(request: HttpRequest) -> JsonResponse:
     coin = json.loads(request.body)
     coin_checker = CoinChecker()
 
-    return JsonResponse({"status": "done"})
-
-
-def watch_dexscreener(request: HttpRequest) -> JsonResponse:
-    """ 
-    Запускает мониторинг и анализ на сайте https://dexscreener.com/
-    """
-    
-    data = json.loads(request.body)
-    filter = data["filter"] if data["filter"] else "?rankBy=trendingScoreH6&order=desc&minLiq=5000&maxAge=1"
-    pages = int(data["pages"]) if data["pages"] else "1"
-    
-    with SB(
-        uc=True, 
-        test=True, 
-        #xvfb=True,
-        headless2=True,
-        extension_dir=settings.CAPTCHA_EXTENSION_DIR
-    ) as sb:
-        dex_parser = DexScreeneWatcher(sb)
-        dex_parser.watch_coins(pages, filter)
-            
     return JsonResponse({"status": "done"})
