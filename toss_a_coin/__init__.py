9d25bdcae3d49a2c4211cfdf21f45ae8a6e5b748
Irina Khaustova <irinakhaus@gmail.com>
2024-09-20 17:33:29 +0300

    Update migrations

--- toss_a_coin/__init__.py
+++
@@ -1,3 +0,0 @@
-# from core.celery import app as celery_app
-
-# __all__ = ('celery_app',)
--- toss_a_coin/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class TossACoinConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'toss_a_coin'
