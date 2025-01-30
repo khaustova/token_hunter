99f39a10c5b007aa8d2e9510218bef2792559377
Irina Khaustova <irinakhaus@gmail.com>
2025-01-30 17:39:47 +0300

    Add spoiler

---
+++ dashboard/static/dashboard/js/spoiler.js
@@ -0,0 +1,13 @@
+// Скрывает/показывает содержимое под спойлером
+
+$(function() {
+	$('div.spoiler-title').click(function() {
+		$(this)
+			.children()
+			.first()
+			.toggleClass('show-icon')
+			.toggleClass('hide-icon');
+		$(this)
+			.parent().children().last().toggle();
+	});
+});
