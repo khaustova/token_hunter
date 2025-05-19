9cf0e17ef312341baee63c990e31cd2e7ae5a39a
Irina Khaustova <irinakhaus@gmail.com>
2025-05-20 00:35:08 +0300

    Style logs

--- token_hunter/src/rugcheck/rugcheck_api.py
+++ token_hunter/src/rugcheck/rugcheck_api.py
@@ -47,16 +47,31 @@ def rugchek_token_with_api(token_address: str) -> dict:
         for risk in token_risks:
             if risk.get("name") not in settings.RUGCHECK_ACCEPTABLE_RISKS:
                 rugcheck_result["risk_level"] = "Bad"
-                logger.info(f"Уровень риска токена {token_address}: BAD, так как риск {risk.get("name")} не является допустимым")
+                logger.info(
+                    (
+                        f"Уровень риска токена {token_address}: BAD, "
+                        f"так как риск {risk.get("name")} не является допустимым"
+                    )
+                )
 
                 return rugcheck_result
 
     if rugcheck_data.get("score_normalised", 100) > settings.RUGCHECK_NORMALISED_SCORE:
         rugcheck_result["risk_level"] = "Bad"
-        logger.info(f"Уровень риска токена {token_address}: BAD, так как {rugcheck_data.get("score_normalised")} > {settings.RUGCHECK_NORMALISED_SCORE}")
+        logger.info(
+            (
+                f"Уровень риска токена {token_address}: BAD, так как "
+                f"{rugcheck_data.get("score_normalised")} > {settings.RUGCHECK_NORMALISED_SCORE})"
+            )
+        )
 
         return rugcheck_result
 
     rugcheck_result["risk_level"] = "Good"
-    logger.info(f"Уровень риска токена {token_address}: GOOD.\nЕго риски: {risks_name}.\nЕго очки: {rugcheck_data.get("score_normalised")}")
+    logger.info(
+        (
+            f"Уровень риска токена {token_address}: GOOD.\nЕго риски: {risks_name}."
+            f"\nЕго очки: {rugcheck_data.get("score_normalised")}"
+        )
+    )
     return rugcheck_result
